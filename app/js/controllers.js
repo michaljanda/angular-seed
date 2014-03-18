2f914fc6748b02242f5742cc2ebffc03aedf97c3
mjanda <michal.janda@artin.cz>
2014-03-18 12:04:35 +0100

    Funkční 2 direktivy

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,13 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+    controller('MyCtrl1', ['$scope', function (sc) {
 
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+    }])
+    .controller('MyCtrl2', ['$scope', function (sc) {
+      sc.car = 'AudiTT';
+      sc.michal = {
+        name: "Michal",
+        adress: "Tišnov"
+      }
+    }]);
--- app/js/directives.js
+++ app/js/directives.js
@@ -4,8 +4,55 @@
 
 
 angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+    directive('appVersion', ['version', function (version) {
+      return function (scope, elm, attrs) {
+        elm.text(version);
+      };
+    }])
+    .directive('myCurrentTime', function ($interval, dateFilter) {
+
+      function link(scope, element, attrs) {
+        var format,
+            timeoutId;
+
+        function updateTime() {
+          element.text(dateFilter(new Date(), format));
+        }
+
+        scope.$watch(attrs.with, function (value) {
+          format = value;
+          updateTime();
+        });
+
+        element.on('$destroy', function () {
+          $interval.cancel(timeoutId);
+        });
+
+        // start the UI update process; save the timeoutId for canceling
+        timeoutId = $interval(function () {
+          updateTime(); // update DOM
+        }, 1000);
+      }
+
+      return {
+        link: link
+      };
+    })
+    .directive('writeTable', function () {
+
+      function link(scope, element, attrs) {
+        scope.$watch(attrs.rows, function (value) {
+          scope.rows = value;
+        });
+      }
+
+      return {
+        link: link,
+        templateUrl: 'partials/directives/simple-table.html',
+        controller: function ($scope, $http) {
+          $http.get('mockdata/example.json').success(function (data) {
+            $scope.data = data;
+          });
+        }
+      };
+    });
